034a0108c8f5a1b5ab600b97e74ad771854f17ff
Tiago Li <pphc@163.com>
2015-01-13 14:54:14 +0800

    added implementations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special vector that is a list of functions to 
+## 1. set the matrix of the vector
+## 2. get the matrix of the vector
+## 3. set the solve of the vector
+## 4. get the solve of the vector
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(s) m <<- s
+    getsolve <- function() m
+    list (set = set, get = get,
+          setsolve = setsolve,
+          getsolve = getsolve)
 }
 
+## cachasolve calculates the solve of the special vector created with makeCacheMatrix.
+## It first checks if the solve has been already calculated. It so, the cache is fetched
+## and returned without calculation. Otherwise, it calculates the solve of the matrix
+## and set the value of the solve via setsolve function
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...){
+    m <- x$getsolve()
+    if (!is.null(m)) {
+        message("getting cached solve")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
